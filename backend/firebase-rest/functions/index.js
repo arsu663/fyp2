e85dd09e1821f9e2c297214cb3b534f5f532b50b
Arsalan <66947580+arsu663@users.noreply.github.com>
2022-05-20 19:03:12 +0800

    Re-setup firebase and implement users rest using real firebase

--- backend/firebase-rest/functions/index.js
+++ backend/firebase-rest/functions/index.js
@@ -1,31 +1,17 @@
 const functions = require("firebase-functions");
 const express = require("express");
-const db = require('./api/databsase')
-
-
-
 const app = express();
+// const todosRouter = require("./api/controllers/todos_controller");
+const usersRouter = require("./api/controller/users_controller");
 
-app.get("/newuser/:name", async (req, res) => {
-    const name = req.params.name;
-    const user = { name: name };
-    const result = await db.create("users", user);
-    user.id = result.id;
-    return res.json(user);
-  });
-  
-  app.get("/deleteuser/:id", async (req, res) => {
-    const userId = req.params.id;
-    const result = await db.delete("users", userId);
-    console.log(result);
-    return res.json(userId);
-  });
-  
-  exports.api = functions.https.onRequest(app);
-  
-  // To handle "Function Timeout" exception
-  exports.functionsTimeOut = functions.runWith({
-    timeoutSeconds: 300,
-  });
+app.use(express.json());
+app.use("/users", usersRouter);
 
-  exports.setupdb = functions.https.onRequest(require("./setup_database"));
+exports.api = functions.https.onRequest(app);
+
+// To handle "Function Timeout" exception
+exports.functionsTimeOut = functions.runWith({
+  timeoutSeconds: 300,
+});
+
+exports.setupdb = functions.https.onRequest(require("./setup_database"));
--- backend/firebase-rest/functions/setup_database.js
+++ backend/firebase-rest/functions/setup_database.js
@@ -1,30 +1,62 @@
-const db = require("./api/databsase");
+// const db = require("../functions/api/database");
+const db = require("./api/database")
 
 async function setupDatabase(req, res, next) {
-  // To delete all the collections
-  const collections = ["users", "appointments"];
-  collections.forEach(async (collection) => await deleteCollection(collection));
+    // To delete all the collections
+    const collections = ["users", "appointments"];
+    collections.forEach(async (collection) => await deleteCollection(collection));
 
-  // Add documents to the todos collection
-  addDocuments("appointments", [
-    { title: "Chest-Xray", completed: true },
-    { title: "Pediatric ", completed: true },
-    { title: "Obgyn appointent", completed: false },
-    { title: "Determalogist appointent", completed: false },
-    { title: "Follow up appointment", completed: true },
-  ]);
+    addDocuments("users", [
+        {
+            name: "Arsalan",
+            password: "test123",
+            email: "arsu@gmail.com",
+            phoneNumber: "123456",
+            imageUrl: "",
+            hasAppointment: false,
+            dateTime: "Tue",
+        },
+        {
+            name: "Adnan",
+            password: "test123",
+            email: "adnan@gmail.com",
+            phoneNumber: "123456",
+            imageUrl: "",
+            hasAppointment: false,
+            dateTime: "Tue",
+        },
+        {
+            name: "Nasir",
+            password: "test123",
+            email: "nasir@gmail.com",
+            phoneNumber: "123456",
+            imageUrl: "",
+            hasAppointment: false,
+            dateTime: "Tue",
+        },
+        {
+            name: "Israr",
+            password: "test123",
+            email: "israr@gmail.com",
+            phoneNumber: "123456",
+            imageUrl: "",
+            hasAppointment: false,
+            dateTime: "Tue",
+        },
 
-  res.send("Setting Up Database.... Done ");
+    ]);
+
+    res.send("Setting Up Database.... Done ");
 }
 
 async function deleteCollection(collection) {
-  const cref = db.firestore.collection(collection);
-  const docs = await cref.listDocuments();
-  docs.forEach((doc) => doc.delete());
+    const cref = db.firestore.collection(collection);
+    const docs = await cref.listDocuments();
+    docs.forEach((doc) => doc.delete());
 }
 
 function addDocuments(collection, docs) {
-  docs.forEach((doc) => db.create(collection, doc));
+    docs.forEach((doc) => db.create(collection, doc));
 }
 
 module.exports = setupDatabase;
